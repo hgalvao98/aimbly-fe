fcc58d8ec07910dc93a1dfa5dc555f80c12631d7
hgalvao98 <henrique.galvao@hotmail.com>
2022-08-17 17:06:07 -0300

    fixed bug

--- src/reducers/events.tsx
+++ src/reducers/events.tsx
@@ -1,13 +1,29 @@
-export const initialState: [] = [];
+export const initialState: [] | any = [];
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default function (state = initialState, action: any) {
   switch (action.type) {
     case "ADD_EVENT":
-      return [...state, { time: action.time, event: action.event }];
+      return [
+        ...state,
+        { time: action.time, event: action.event, completed: false },
+      ];
     case "REMOVE_EVENT":
-      const events = state.filter((data) => data !== action.payload);
+      const events = state.filter((data: any) => data !== action.payload);
       return events;
+    case "DEL_EVENT":
+      console.log(state);
+      // eslint-disable-next-line array-callback-return
+      return state.map((item: any) => {
+        if (item.event === action.payload.event) {
+          return {
+            ...item,
+            completed: !item.completed,
+          };
+        }
+        return item;
+      });
+
     default:
       return state;
   }
